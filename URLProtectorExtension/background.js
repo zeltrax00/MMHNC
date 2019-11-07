86889d08b346698862194c15e405f743352116b0
Tung Nguyen <32799754+zeltrax00@users.noreply.github.com>
2019-11-08 00:10:29 +0700

    Add files via upload

--- URLProtectorExtension/background.js
+++ URLProtectorExtension/background.js
@@ -4,24 +4,21 @@ function listener(requestDetails) {
     let safe = "true";
     // Check các kiểu rồi gán kết quả cho biến safe
     var req = new XMLHttpRequest();
-
-    req.open('GET', 'http://localhost:5000/', false);
-
-    safe = req.send(requestDetails.url);
-
+    req.open('GET', 'http://localhost:5000/?url=' + requestDetails.url, false);
+    req.send(null);
+    safe = req.responseText;
     if (safe == "true")
         return {cancel: false};
     else if (safe == "false")
     {
         let c = confirm("Trang web " + requestDetails.url + " có dấu hiệu không an toàn, bạn có muốn tiếp tục ?");
-        
         if (c == true)
             return {cancel: false};
         else
             return {cancel: true};
     }
     else
-        console.log("Error from server: " + safe);
+        console.log("Error server: " + req.responseText);
 }
 
 chrome.webRequest.onBeforeRequest.addListener(
